61ad19c2f04ac78f8ca9829fc2b940d4478f876e
Natalia Malcova <nataliamalkova@yahoo.com>
2019-11-04 14:46:11 -0500

    added instructions for the app into README file

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Psychic-Game
+
+
+## Guess what letter I'm thinking of
+
+It is a guessing game that randomly picks a letter, and the user has to guess which letter the app chose.
+
+1. Wins: (# of times the user has guessed the letter correctly)
+
+2. Losses: (# of times the user has failed to guess the letter correctly after exhausting all guesses)
+
+3. Guesses Left: (# of guesses left. This will update)
+
+4. Your Guesses So Far: (the specific letters that the user typed. Display these until the user either wins or loses.)
+
+5. When the player wins, increase the Wins counter and start the game over again (without refreshing the page).
+
+6. When the player loses, increase the Losses counter and restart the game without a page refresh (just like when the user wins).
+
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,27 @@
 
 
-var letterOptions = ["a","b","d","e","k","m","p","r","u","v","x","y"];     //to be matched   
+var letterOptions = ["a","b","d","e","k","m","p","r","u","v","x","y"];     //options to be matched   
 
-var winCount =0;
-var lossCount =0;
+var winCount = 0;
+var lossCount = 0;
 var guessesLeft = 10;
-var userGuess ="";
+var userGuess = "";
 var computerGuess =letterOptions[Math.floor(Math.random()*letterOptions.length)];
 
 function wrongGuesses (userGuess){
-    console.log(userGuess, 'gtgtg');
+    console.log(userGuess);
     document.getElementById("wrongGuesses").innerHTML = userGuess;
 }
 
 function reStartGame () {
-    winCount =0;
-    lossCount =0;
+    winCount = 0;
+    lossCount = 0;
     guessesLeft = 10;
-    userGuess ="";
+    userGuess = "";
     computerGuess =letterOptions[Math.floor(Math.random()*letterOptions.length)];
 }
 reStartGame();
+
 
 document.onkeyup = function() {
     var newUserGuess =  String.fromCharCode(event.keyCode).toLowerCase();
@@ -29,13 +30,15 @@ document.onkeyup = function() {
 
     if (newUserGuess == computerGuess) {
         winCount++;
+        // if(guessesLeft === 0) {
+        //     reStartGame();
+        // }
         document.getElementById("winCounter").innerHTML = winCount;
     }
     if (newUserGuess !== computerGuess) {
         lossCount++;
         if(guessesLeft === 0){
             reStartGame();
-            //logic to restart game
         }
         guessesLeft--;
         document.getElementById("lossCounter").innerHTML = lossCount;
